c0956152084047add2616bad0e614a324c100988
Saku Penttilä <sakuinenpenttila@gmail.com>
2024-04-20 23:42:47 +0300

    R3F, Model, lemans.glb, gsap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Environment } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei'
+import Model from './components/Model';
 
-function App() {
+
+
+
+
+export default function App() {
+  const objectRef = useRef();
+  const glRef = useRef();
+  const canvasRef = useRef();
+  const cameraRef = useRef();
+  
+  const getObjectRef = (object) => {
+    objectRef.current = object;
+  };
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='canvas'>
+      <Canvas
+        id="myCanvas"
+        gl={{ alpha:true, antialias: true }}
+        style={{ width: '100vw', height: '50vh' } }
+        camera={{  position: [0, 0, 4], fov: 60, near: 0.1, far: 20 }}
+        onCreated={({ gl, camera }) => {
+          glRef.current = gl; // Save gl to useRef
+          cameraRef.current = camera; // Save camera to useRef
+        }}
+        ref={canvasRef}
+      >
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[0, 3, 5]} />
+        <Suspense fallback={null}>
+          <Environment preset='forest' />
+          <Model id="model" name={"Laatikko.glb"} onObjectLoad={getObjectRef} camRef={cameraRef} glRef={glRef}/>
+        </Suspense>
+        <OrbitControls enableZoom={false} enablePan={false} />
+      </Canvas>
+      </div>
   );
 }
 
-export default App;
---
+++ src/components/Model.js
@@ -0,0 +1,80 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from 'gsap';
+import { useGLTF } from '@react-three/drei';
+import { useThree } from '@react-three/fiber';
+import LaatikkoModel from "./lemans.glb"
+//https://sketchfab.com/3d-models/low-poly-car-pontiac-le-mans-1971-86ec22eeceee4cd78ef10352c8d0e6d9 Model for low poly car, thanks Calipo;
+
+gsap.registerPlugin(ScrollTrigger); 
+
+
+export default function Model(props, glRef, camRef ) {
+
+    const objectRef = useRef();
+    const object = useGLTF(LaatikkoModel); 
+
+    const handleObjectRef = (p) => {
+     props.onObjectLoad(p)
+    }
+
+ 
+    
+    useEffect(() => {
+      // Preload the GLTF model if necessary
+      useGLTF.preload(LaatikkoModel);
+  }, [object]);
+
+
+  
+    useEffect(() => {
+      if (object.scene) {
+        object.scene.position.set(-10, 0.5, 0);
+        object.scene.rotation.set(0.2, 1.8, 0);
+        handleObjectRef(object.scene);
+      }
+    }, [props.name]);
+
+    
+
+    useEffect(() => {
+
+        const tlPos = gsap.timeline({
+            scrollTrigger: {
+              scrub: 1,
+              trigger: ".canvas",
+              start: "top-=150%",
+              end: "bottom-=150%",
+            },
+          });
+          tlPos.to(object.scene.position, {
+            x: 0,
+          })
+
+          const ohjeetPos = gsap.timeline({
+            scrollTrigger: {
+              scrub: 1,
+              trigger: ".canvas",
+              start: "bottom-=145%",
+              end: "bottom-=150%"
+            },
+          });
+
+          /* Move camera
+          tlRot.to(cameraRef.current.position, {
+            x:4,
+            duration: 3
+          }) */
+    
+        return () => {
+          // Cleanup
+          ScrollTrigger.getAll().forEach((trigger) => {
+            trigger.kill(); // Kill all ScrollTriggers to prevent memory leaks
+          });
+        };
+      }, [objectRef.current]);
+
+  return (
+    <primitive object={object.scene} />
+  );
+}
